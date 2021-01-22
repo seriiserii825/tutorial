d3469011882b88a1aa567b306f896333f0b520b5
serii <seriiburduja@gmail.com>
2021-01-22 12:33:22 +0200

    single product query

--- src/pages/products.js
+++ src/pages/products.js
@@ -24,25 +24,26 @@ const query = graphql`
 
 const Products = () => {
   const data = useStaticQuery(query)
-  const products = data.allContentfulProduct.nodes.map(item => {
-    return (
-      <div className="products__item" key={item.title}>
-        <Image fluid={item.image.fluid} />
-        <div className="products__info">
-          <h2 className="products__title">{item.title}</h2>
-          <div className="price">{item.price}</div>
-        </div>
-        <Link to={`/products/${item.slug}`}>More details</Link>
-      </div>
-    )
-  })
 
   return (
     <div className="products">
       <Layout>
         <div className="container">
           <h2 className="products-page__title">Products</h2>
-          <div className="products__wrap">{products}</div>
+          <div className="products__wrap">
+            {data.allContentfulProduct.nodes.map(item => {
+              return (
+                <div className="products__item" key={item.title}>
+                  <Image fluid={item.image.fluid} />
+                  <div className="products__info">
+                    <h2 className="products__title">{item.title}</h2>
+                    <div className="price">{item.price}</div>
+                  </div>
+                  <Link to={`/products/${item.slug}`}>More details</Link>
+                </div>
+              )
+            })}
+          </div>
         </div>
       </Layout>
     </div>
